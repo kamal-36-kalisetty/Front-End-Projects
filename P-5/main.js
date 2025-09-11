a46e83ce70e758f3c9efb96804d7a8808de37861
kamal-36-kalisetty <kamalkalisettykk@gmail.com>
2025-09-11 13:06:42 +0530

    Add files via upload

---
+++ P-5/main.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("hamburger");
+const list = document.getElementById("navlist");
+
+burger.addEventListener("click",()=>{
+    list.classList.toggle("navlist-active")
+})
